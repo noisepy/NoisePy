bec203af058e0934c9d8c9646de4efadf1f687f0
chengxin <chengxinjiang@gmail.com>
2019-03-04 20:15:27 -0500

    update plot module

--- plot_modules.py
+++ plot_modules.py
@@ -10,7 +10,7 @@ the main purpose of this module is to assemble some simple functions
 to plot different waveform types from intermediate files outputed by 
 the Noise_Python package
 
-Chengxin Jiang [Mar.04.2019]
+Chengxin Jiang (Mar.04.2019)
 
 it includes:
 1). plot_spectrum(sfile,iday,icomp) to plot the FFT files for each station
@@ -24,15 +24,20 @@ it includes:
 def plot_spectrum(sfile,iday,icomp):
     '''
     this script plots the noise spectrum for the idayth on icomp (results from step1)
+    and compare it with the waveforms in time-domain
     '''
     dt = 0.05
     sta = sfile.split('/')[-1].split('.')[1]
     ds = pyasdf.ASDFDataSet(sfile,mode='r')
     comp = ds.auxiliary_data.list()
+    
+    #--check whether it exists----
     if icomp in comp:
         tlist = ds.auxiliary_data[icomp].list()
         if iday in tlist:
             spect = ds.auxiliary_data[icomp][iday].data[:]
+
+            #---look at hourly----
             if spect.ndim==2:
                 nfft = spect.shape[1]
                 for ii in range(spect.shape[0]):
@@ -45,6 +50,8 @@ def plot_spectrum(sfile,iday,icomp):
                     plt.plot(np.arange(0,nfft)*dt,waveform,'k-')
                     plt.plot([0,nfft*dt],[0,0],'r--',linewidth=1)
                     plt.show()
+
+            #----look at stacked daily----
             else:
                 nfft  = len(spect)
                 waveform = np.real(np.fft.irfft(spect)[0:nfft])
@@ -68,6 +75,7 @@ def plot_moveout(sfile,freqmin,freqmax,net1=None,sta1=None,comp1=None):
            plot_moveout('2010_12_18.h5',0.1,0.3,'E','HNOM','HNU')
     '''
     
+    #---basic parameters----
     #freqmin=0.2
     #freqmax=0.5
     maxlag = 100
@@ -86,18 +94,23 @@ def plot_moveout(sfile,freqmin,freqmax,net1=None,sta1=None,comp1=None):
             mdist = 0
             plt.figure(figsize=(9,6))
 
+            #---loop through all receivers-----
             for ireceiver in rlist:
                 sta2 = ireceiver.split('s')[1]
                 comp2 = ireceiver.split('s')[2]
+                #--plot all 3 cross-component---
                 if comp2[2] == 'E':
-                        color = 'r-'
+                    color = 'r-'
                 elif comp2[2] == 'N':
-                        color = 'g-'
+                    color = 'g-'
                 else:
-                        color = 'b-'
+                    color = 'b-'
+
+                #---------read parameters and copy data-----------
                 dist = ds.auxiliary_data[isource][ireceiver].parameters['dist']
-                data = ds.auxiliary_data[isource][ireceiver].data[:]*2.5/max(ds.auxiliary_data[isource][ireceiver].data[:])
+                data = ds.auxiliary_data[isource][ireceiver].data[:]
                 data = bandpass(data,freqmin,freqmax,int(1/dt),corners=4, zerophase=True)
+                data = data*2.5/max(data)
                 npts = len(data)
                 indx0 = npts//2
                 tindx = int(maxlag/dt)
@@ -107,12 +120,15 @@ def plot_moveout(sfile,freqmin,freqmax,net1=None,sta1=None,comp1=None):
                 plt.ylabel('offset (km)')
                 plt.text(maxlag*0.9,dist,sta2,fontsize=6)
 
+                #----use to plot o times------
                 if mdist < dist:
                     mdist = dist
             plt.plot([0,0],[0,mdist],'r--',linewidth=0.8)
             plt.legend(['E','N','Z'],loc='upper right')
             plt.show()
     else:
+
+        #----loop through all sources------
         for isource in slist:
             sta1  = isource.split('s')[1]
             comp1 = isource.split('s')[2]
@@ -120,19 +136,24 @@ def plot_moveout(sfile,freqmin,freqmax,net1=None,sta1=None,comp1=None):
             mdist = 0
             plt.figure(figsize=(9,6))
 
+            #---for all receivers-----
             for ireceiver in rlist:
                 sta2 = ireceiver.split('s')[1]
                 comp2 = ireceiver.split('s')[2]
-                if comp2[2] == 'E':
-                        color = 'r-'
-                elif comp2[2] == 'N':
-                        color = 'g-'
-                else:
-                        color = 'b-'
 
+                #---plot all 3 cross-component----
+                if comp2[2] == 'E':
+                    color = 'r-'
+                elif comp2[2] == 'N':
+                    color = 'g-'
+                else:
+                    color = 'b-'
+
+                #------------get parameters and copy data-------------
                 dist = ds.auxiliary_data[isource][ireceiver].parameters['dist']
-                data = ds.auxiliary_data[isource][ireceiver].data[:]*2.5/max(ds.auxiliary_data[isource][ireceiver].data[:])
+                data = ds.auxiliary_data[isource][ireceiver].data[:]
                 data = bandpass(data,freqmin,freqmax,int(1/dt),corners=4, zerophase=True)
+                data = data*2.5/max(data)
                 npts = len(data)
                 indx0 = npts//2
                 tindx = int(maxlag/dt)
@@ -153,10 +174,14 @@ def plot_cc_2lags(sfile,freqmin,freqmax,net1,sta1,comp1,net2=None,sta2=None,comp
     '''
     this script plots the two lags of the cross-correlation functions to compare
     the symmetry of the waveforms
+
+    usage: plot_cc_2lags('2010_12_20.h5',0.1,0.3,'E','AYHM','HNU') or
+           plot_cc_2lags('2010_12_20.h5',0.1,0.3,'E','AYHM','HNU','E','YKKM','HNU')
     '''
+    #---basic parameters---
     #freqmin=0.1
     #freqmax=0.3
-    maxlag = 150
+    maxlag = 100
     dt = 0.05
     tt = np.arange(0, maxlag/dt+1)*dt
 
@@ -165,16 +190,19 @@ def plot_cc_2lags(sfile,freqmin,freqmax,net1,sta1,comp1,net2=None,sta2=None,comp
     isource = net1+'s'+sta1+'s'+comp1
 
     if isource in slist:
+        #-----when receiver info is provided-----
         rlist = ds.auxiliary_data[isource].list()
         if net2 and sta2 and comp2:
             ireceiver = net2+'s'+sta2+'s'+comp2
             if ireceiver in rlist:
+                #--------read dist info to infer travel time window---------
                 dist = ds.auxiliary_data[isource][ireceiver].parameters['dist']
                 data = ds.auxiliary_data[isource][ireceiver].data[:]
                 data = bandpass(data,freqmin,freqmax,int(1/dt),corners=4, zerophase=True)
                 npts = len(data)
                 indx0 = npts//2
                 tindx = int(maxlag/dt)
+                #-------positive lag and flip negative lags----------
                 plt.plot(tt,data[indx0:indx0+tindx+1],'r-',linewidth=1)
                 plt.plot(tt,np.flip(data[indx0-tindx:indx0+1]),'g-',linewidth=1)
                 plt.title('%s_%s_%s_%s %6.1f km @%4.1f-%4.1f Hz' % (sta1,comp1,sta2,comp2,dist,freqmin,freqmax))
@@ -183,6 +211,7 @@ def plot_cc_2lags(sfile,freqmin,freqmax,net1,sta1,comp1,net2=None,sta2=None,comp
                 plt.show()
         else:
             for ir in range(len(rlist)//3):
+                #----plot all 3 components-----
                 ireceiver = rlist[ir*3]
                 sta2 = ireceiver.split('s')[1]
                 dist = ds.auxiliary_data[isource][ireceiver].parameters['dist']
@@ -197,19 +226,106 @@ def plot_cc_2lags(sfile,freqmin,freqmax,net1,sta1,comp1,net2=None,sta2=None,comp
                 npts = len(data1)
                 indx0 = npts//2
                 tindx = int(maxlag/dt)
+                #-------plot positive lag and flip negative lags----------
                 plt.subplot(311)
-                plt.plot(tt,data1[indx0:indx0+tindx+1]/max(data1[indx0:indx0+tindx+1]),'r-',linewidth=1)
-                plt.plot(tt,np.flip(data1[indx0-tindx:indx0+1])/max(data1[indx0-tindx:indx0+1]),'g-',linewidth=1)
+                plt.plot(tt,data1[indx0:indx0+tindx+1]/max(data1),'r-',linewidth=1)
+                plt.plot(tt,np.flip(data1[indx0-tindx:indx0+1])/max(data1),'g-',linewidth=1)
                 plt.title('%s_%s_%s_[ENZ] %6.1f km @%4.1f-%4.1f Hz' % (sta1,comp1,sta2,dist,freqmin,freqmax))
                 plt.legend(['positive','negative'],loc='upper right')
+                #----3 cross components---
                 plt.subplot(312)
-                plt.plot(tt,data2[indx0:indx0+tindx+1]/max(data2[indx0:indx0+tindx+1]),'r-',linewidth=1)
-                plt.plot(tt,np.flip(data2[indx0-tindx:indx0+1])/max(data2[indx0-tindx:indx0+1]),'g-',linewidth=1)
+                plt.plot(tt,data2[indx0:indx0+tindx+1]/max(data2),'r-',linewidth=1)
+                plt.plot(tt,np.flip(data2[indx0-tindx:indx0+1])/max(data2),'g-',linewidth=1)
                 plt.subplot(313)
-                plt.plot(tt,data3[indx0:indx0+tindx+1]/max(data3[indx0:indx0+tindx+1]),'r-',linewidth=1)
-                plt.plot(tt,np.flip(data3[indx0-tindx:indx0+1])/max(data3[indx0-tindx:indx0+1]),'g-',linewidth=1)
+                plt.plot(tt,data3[indx0:indx0+tindx+1]/max(data3),'r-',linewidth=1)
+                plt.plot(tt,np.flip(data3[indx0-tindx:indx0+1])/max(data3),'g-',linewidth=1)
                 plt.xlabel('time [s]')            
                 plt.show()
+
+
+def plot_cc_withtime(ccfdir,freqmin,freqmax,net1,sta1,comp1,net2=None,sta2=None,comp2=None):
+    '''
+    plot the filtered cross-correlation functions between station-pair sta1-sta2
+    for all of the available days stored in ccfdir
+
+    '''
+    #---basic parameters----
+    maxlag = 100
+    dt = 0.05
+    tt = np.arange(-maxlag/dt, maxlag/dt+1)*dt
+
+    afiles = sorted(glob.glob(os.path.join(ccfdir,'*.h5')))
+    source = net1+'s'+sta1+'s'+comp1
+
+    #----when recevier is known----
+    if net2 and sta2 and comp2:
+        recever= net2+'s'+sta2+'s'+comp2
+
+        #---loop through each day------
+        for ii in range(len(afiles)):
+            
+            #--maximum 50 days one plot----
+            if ii%50==0:
+                plt.figure(figsize=(9,6))
+
+            with pyasdf.ASDFDataSet(afiles[ii],mode='r') as ds:
+                dist = ds.auxiliary_data[source][recever].parameters['dist']
+                iday = afiles[ii].split('/')[-1].split('.')[0]
+                data = ds.auxiliary_data[source][recever].data[:]
+                data = bandpass(data,freqmin,freqmax,int(1/dt),corners=4, zerophase=True)
+                #--normalize the data----
+                data = data/max(data)
+                npts = len(data)
+                #----make index----
+                indx0 = npts//2
+                tindx = int(maxlag/dt)
+                plt.plot(tt,data[indx0-tindx:indx0+tindx+1]+ii*2,'k-',linewidth=0.5)
+                plt.text(maxlag*0.9,ii*2,iday,fontsize=6)
+
+        plt.grid(True)
+        plt.plot([0,0],[0,ii*2],'r--',linewidth=1.5)
+        plt.title('%s_%s_%s_%s dist %6.1f @%4.1f-%4.1f Hz' % (sta1,comp1,sta2,comp2,dist,freqmin,freqmax))
+        plt.xlabel('time [s]')
+        plt.ylabel('days')
+        plt.show()
+    
+    else:
+        #------when receiver info is not known---------
+        with pyasdf.ASDFDataSet(afiles[0],mode='r') as ds:
+            rlist = ds.auxiliary_data[source].list()E
+            dist = ds.auxiliary_data[source][rlist[0]].parameters['dist']
+        
+        for recever in rlist:
+            net2 = recever.split('s')[0]
+            sta2 = recever.split('s')[1]
+            comp2= recever.split('s')[2]
+
+            #---loop through each day------
+            for ii in range(len(afiles)):
+                
+                #--maximum 50 days one plot----
+                if ii%50==0:
+                    plt.figure(figsize=(9,6))
+
+                with pyasdf.ASDFDataSet(afiles[ii],mode='r') as ds:
+                    iday = afiles[ii].split('/')[-1].split('.')[0]
+                    data = ds.auxiliary_data[source][recever].data[:]
+                    data = bandpass(data,freqmin,freqmax,int(1/dt),corners=4, zerophase=True)
+                    #--normalize the data----
+                    data = data/max(data)
+                    npts = len(data)
+                    #----make index----
+                    indx0 = npts//2
+                    tindx = int(maxlag/dt)
+                           
+                    plt.plot(tt,data[indx0-tindx:indx0+tindx+1]+ii*2,'k-',linewidth=0.5)
+                    plt.text(maxlag*0.9,ii*2,iday,fontsize=6)
+            plt.grid(True)
+            plt.plot([0,0],[0,ii*2],'r--',linewidth=1.5)
+            plt.title('%s_%s_%s_%s dist %6.1f @%4.1f-%4.1f Hz' % (sta1,comp1,sta2,comp2,dist,freqmin,freqmax))
+            plt.xlabel('time [s]')
+            plt.ylabel('days')
+            plt.show()
 
 
 def compare_c2_c3_waveforms(c2file,c3file,maxlag,c2_maxlag,dt):
