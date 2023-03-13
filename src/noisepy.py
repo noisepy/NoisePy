c35215fc8bdac619210cc57a854291c2da7973cb
Carlos Garcia Jurado Suarez <carlosg@uw.edu>
2023-03-13 13:44:37 -0700

    Update tutorial to use requirements.txt and noisepy.py

--- src/noisepy.py
+++ src/noisepy.py
@@ -8,6 +8,8 @@ from S0A_download_ASDF_MPI import download
 # Utility running the different steps from the command line. Defines the arguments for each step
 
 DATE_FORMAT = '%%Y_%%m_%%d_%%H_%%M_%%S'
+default_start_date = "2016_07_01_0_0_0"                               # start date of download
+default_end_date   = "2016_07_02_0_0_0"                               # end date of download
 
 class Step(Enum):
     DOWNLOAD = 1
@@ -27,11 +29,11 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(dest='step', required=True)
     
     # Download arguments
-    down_parser = subparsers.add_parser(Step.DOWNLOAD.name.lower())
+    down_parser = subparsers.add_parser(Step.DOWNLOAD.name.lower(), formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     down_parser.add_argument("--path", type=str, default=os.path.join(os.path.expanduser('~'), 'Documents/SCAL'), help="Directory for downloading files")
-    down_parser.add_argument("--start", type=valid_date, required=True, help="Start date in the format: "+DATE_FORMAT)
-    down_parser.add_argument("--end",   type=valid_date, required=True, help="End date in the format: "+DATE_FORMAT)
-    down_parser.add_argument("--stations", type=lambda s: s.split(","), help="Comma separated list of stations or '*' (default)", default="*")
+    down_parser.add_argument("--start", type=valid_date, required=True, help="Start date in the format: "+DATE_FORMAT, default=default_start_date)
+    down_parser.add_argument("--end",   type=valid_date, required=True, help="End date in the format: "+DATE_FORMAT, default=default_end_date)
+    down_parser.add_argument("--stations", type=lambda s: s.split(","), help="Comma separated list of stations or '*' for all", default="*")
     down_parser.add_argument("--channels", type=lambda s: s.split(","), help="Comma separated list of channels", default="BHE,BHN,BHZ")
     down_parser.add_argument("--inc_hours", type=int, default=24, help="Time increment size (hrs)")
     args = parser.parse_args()
