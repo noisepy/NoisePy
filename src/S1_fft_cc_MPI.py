Fix: rFFT and FFT inconsistency leads to broadcast error (issue #50)

src/S1_fft_cc_MPI.py
@@ -47,7 +47,6 @@ rootpath  = os.path.join(os.path.expanduser('~'), 'Documents/SCAL')         # ro
 
 # some control parameters
 input_fmt   = 'h5'                                                          # string: 'h5', 'sac','mseed'
-freq_norm   = 'rma'                                                         # 'no' for no whitening, or 'rma' for running-mean average, 'phase_only' for sign-bit normalization in freq domain.
 time_norm   = 'no'                                                          # 'no' for no normalization, or 'rma', 'one_bit' for normalization in time domain
 cc_method   = 'xcorr'                                                       # 'xcorr' for pure cross correlation, 'deconv' for deconvolution; FOR "COHERENCY" PLEASE set freq_norm to "rma", time_norm to "no" and cc_method to "xcorr"
 flag        = True                                                          # print intermediate variables and computing time for debugging purpose
@@ -78,7 +77,22 @@ max_over_std = 10                                                           # th
 # maximum memory allowed per core in GB
 MAX_MEM = 4.0
 
-def cross_correlate(rootpath: str):
+def resize_fft(fft: np.array, n: int, nfft2: int) -> np.array:
+    # check the size to see if this is an rFFT (half) and resize accordingly
+    if fft.size // 2 == n*nfft2:
+        fft = fft[0:(int(fft.size/2))]
+    return fft.reshape(n, nfft2)
+
+
+def cross_correlate(rootpath: str, freq_norm: str):
+    """
+        Perform the cross-correlation analysis
+
+            Parameters:
+                    rootpath (str): Directory to load data from
+                    freq_norm (int): 'no' for no whitening, or 'rma' for running-mean average, 'phase_only' for sign-bit normalization in freq domain.
+    """
+
     CCFDIR    = os.path.join(rootpath,'CCF')                                    # dir to store CC data
     DATADIR   = os.path.join(rootpath,'RAW_DATA')                               # dir where noise data is located
     locations = os.path.join(DATADIR,'station.txt')                             # station info including network,station,channel,latitude,longitude,elevation: only needed when input_fmt is not h5 for asdf
@@ -325,8 +339,8 @@ def cross_correlate(rootpath: str):
             t0=time.time()
             #-----------get the smoothed source spectrum for decon later----------
             sfft1 = noise_module.smooth_source_spect(fc_para,fft1)
-            sfft1_halved= sfft1[0:(int(sfft1.size/2))]
-            sfft1 = sfft1_halved.reshape(N,Nfft2)
+            sfft1 = resize_fft(sfft1, N, Nfft2)
+
             t1=time.time()
             if flag:
                 print('smoothing source takes %6.4fs' % (t1-t0))
@@ -362,8 +376,7 @@ def cross_correlate(rootpath: str):
                 if not fft_flag[iiR]: continue
 
                 fft2 = fft_array[iiR]
-                fft2_halved= fft2[0:(int(fft2.size/2))]
-                sfft2 = fft2_halved.reshape(N,Nfft2)
+                sfft2 = resize_fft(fft2, N,Nfft2)
                 receiver_std = fft_std[iiR]
 
                 #---------- check the existence of earthquakes ----------