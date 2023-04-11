cc4bf9e9598afd915a83177becb6f9844b21921a
carlosgjs <carlosgjs@users.noreply.github.com>
2023-04-11 13:31:17 -0700

    Add pypi publishing to the release workflow (#80)
    * Restructure files for packaging
    
    * Move application_modules files for packaging
    
    * delete .DS_Store hidden file
    
    * Making noisepy a PyPi package
    
    * fix cli
    
    * rename noisepy to main
    
    * Fix integration tests
    
    * install dev dependencies
    
    * Update readme and python version
    
    * delete setup.py
    
    * gitignore /dist
    
    * Update docstring
    
    * Update contributing instructions
    
    * Remove hatchling from dev dependencies
    
    * REmove wheel dependency
    
    * Publish package to PyPi as part of the release workflow
    
    * checkout repo
    
    * Undo release changes
    
    * roll back version
    
    * Add pypi jobs to release workflow
    
    * add mpi pre-req
    
    * Bump version for testing
    
    * bump version and debug statements
    
    * ver bump
    
    * ver 0.5.8
    
    * Sleep before building docker
    
    * Use latest version from source
    
    * use source version
    
    * Use wheel install for version
    
    * fix exported var

--- src/noisepy/seis/__init__.py
+++ src/noisepy/seis/__init__.py
@@ -3,7 +3,7 @@ from .S0A_download_ASDF_MPI import download  # noqa: F401
 from .S1_fft_cc_MPI import cross_correlate  # noqa: F401
 from .S2_stacking import stack  # noqa: F401
 
-__version__ = "0.5.4"
+__version__ = "0.5.13"
 """
 NoisePy is a Python package designed for fast and easy computation of ambient noise cross-correlation functions.
 It provides additional functionality for noise monitoring and surface wave dispersion analysis.
