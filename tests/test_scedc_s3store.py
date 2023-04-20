c7a10c99ff3fe757806a874a0b50f823510e322d
carlosgjs <carlosgjs@users.noreply.github.com>
2023-04-20 08:49:27 -0700

    SCEDC store implementation (#86)
    * Base data types and ASDFStore implementation
    
    * fix logging
    
    * add tests/fix channeltype use
    
    * add missing dependency
    
    * merge
    
    * SCEDC store implementation
    
    * Add ChannelData, use UTC
    
    * Use ChannelData and UTC
    
    * Update test
    
    * clean up

---
+++ tests/test_scedc_s3store.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timezone
+
+import pytest
+from datetimerange import DateTimeRange
+from obspy import Inventory
+
+from noisepy.seis.scedc_s3store import SCEDCS3DataStore
+
+timespan1 = DateTimeRange(datetime(2022, 1, 2, tzinfo=timezone.utc), datetime(2022, 1, 3, tzinfo=timezone.utc))
+timespan2 = DateTimeRange(datetime(2021, 2, 3, tzinfo=timezone.utc), datetime(2021, 2, 4, tzinfo=timezone.utc))
+files_dates = [
+    ("CIGMR__LHN___2022002.ms", timespan1),
+    ("CIGMR__LHN___2021034.ms", timespan2),
+]
+
+
+@pytest.mark.parametrize("file,expected", files_dates)
+def test_parsefilename(file: str, expected: DateTimeRange):
+    assert expected == SCEDCS3DataStore._parse_timespan(file)
+
+
+@pytest.fixture
+def store():
+    import os
+
+    key = SCEDCS3DataStore._get_cache_key([timespan1])
+    # This avoids the call to the SCEDC service during unit testing
+    # by populating an in-memory cache with an empty Inventory
+    cache = {key: Inventory()}
+    return SCEDCS3DataStore(os.path.join(os.path.dirname(__file__), "./data/s3scedc"), cache)
+
+
+read_channels = [
+    (SCEDCS3DataStore._parse_channel("BKTHIS_LHZ00_2022002.ms")),
+    (SCEDCS3DataStore._parse_channel("CIFOX2_LHZ___2022002.ms")),
+    (SCEDCS3DataStore._parse_channel("CINCH__LHZ___2022002.ms")),
+]
+
+
+@pytest.mark.parametrize("channel", read_channels)
+def test_read(store: SCEDCS3DataStore, channel: str):
+    chdata = store.read_data(timespan1, channel)
+    assert chdata.sampling_rate == 1.0
+    assert chdata.start_timestamp >= timespan1.start_datetime.timestamp()
+    assert chdata.start_timestamp < timespan1.end_datetime.timestamp()
+    assert chdata.data.size > 0
+
+
+def test_timespan_channels(store: SCEDCS3DataStore):
+    timespans = store.get_timespans()
+    assert len(timespans) == 1
+    assert timespans[0] == timespan1
+    channels = store.get_channels(timespan1)
+    assert len(channels) == 3
+    channels = store.get_channels(timespan2)
+    assert len(channels) == 0
