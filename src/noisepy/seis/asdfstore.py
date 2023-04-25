a701bd751e9aac0323d6199ef43c80e1f5141422
carlosgjs <carlosgjs@users.noreply.github.com>
2023-04-25 14:31:48 -0700

    ASDFCCStore: Remove use of tmp file (#92)
    * ASDFCCStore: Remove use of tmp file
    
    * cleanup

--- src/noisepy/seis/asdfstore.py
+++ src/noisepy/seis/asdfstore.py
@@ -11,7 +11,7 @@ import pyasdf
 from datetimerange import DateTimeRange
 
 from . import noise_module
-from .constants import DATE_FORMAT, DONE_MARKER
+from .constants import DATE_FORMAT, DONE_PATH, PROGRESS_DATATYPE
 from .datatypes import Channel, ChannelData, ChannelType, ConfigParameters, Station
 from .stores import CrossCorrelationDataStore, RawDataStore
 
@@ -69,14 +69,15 @@ class ASDFCCStore(CrossCorrelationDataStore):
         self.directory = directory
         Path(directory).mkdir(exist_ok=True)
 
+    # CrossCorrelationDataStore implementation
     def contains(
         self, timespan: DateTimeRange, src_chan: Channel, rec_chan: Channel, parameters: ConfigParameters
     ) -> bool:
-        key = get_channel_pair_key(src_chan, rec_chan)
-        exists = self._tmp_contains(timespan, key)
-        if exists:
-            logger.info(f"Cross-correlation {key} already exists")
-        return exists
+        station_pair = self._get_station_pair(src_chan, rec_chan)
+        contains = self._contains(timespan, station_pair)
+        if contains:
+            logger.info(f"Cross-correlation {station_pair} already exists")
+        return contains
 
     def save_parameters(self, parameters: ConfigParameters):
         fc_metadata = os.path.join(self.directory, "fft_cc_data.txt")
@@ -95,51 +96,49 @@ class ASDFCCStore(CrossCorrelationDataStore):
         cc_params: Dict[str, Any],
         corr: np.ndarray,
     ):
-        filename = f"{os.path.join(self.directory, self._get_filename(timespan))}.h5"
-        with pyasdf.ASDFDataSet(filename, mpi=False) as ccf_ds:
-            # source-receiver pair
-            data_type = f"{src_chan.station}_{rec_chan.station}"
-            path = f"{src_chan.type}_{rec_chan.type}"
-            data = np.zeros(corr.shape, dtype=corr.dtype)
-            data[:] = corr[:]
-            ccf_ds.add_auxiliary_data(data=data, data_type=data_type, path=path, parameters=cc_params)
+        # source-receiver pair: e.g. CI.ARV_CI.BAK
+        station_pair = self._get_station_pair(src_chan, rec_chan)
+        # channels, e.g. bhn_00_bhn_00
+        channels = f"{src_chan.type}_{rec_chan.type}"
+        data = np.zeros(corr.shape, dtype=corr.dtype)
+        data[:] = corr[:]
+        self._add_aux_data(timespan, cc_params, station_pair, channels, data)
 
-        # record the progress
-        self._append_tmp(timespan, get_channel_pair_key(src_chan, rec_chan))
+    def mark_done(self, timespan: DateTimeRange):
+        self._add_aux_data(timespan, {}, PROGRESS_DATATYPE, DONE_PATH, np.zeros(0))
 
     def is_done(self, timespan: DateTimeRange):
-        done = self._tmp_contains(timespan, DONE_MARKER)
+        done = self._contains(timespan, PROGRESS_DATATYPE, DONE_PATH)
         if done:
             logger.info(f"Timespan {timespan} already computed")
         return done
 
-    def mark_done(self, timespan: DateTimeRange):
-        self._append_tmp(timespan, DONE_MARKER)
-
     def read(self, chan1: Channel, chan2: Channel, start: datetime, end: datetime) -> np.ndarray:
         pass
 
-    def _get_filename(self, timespan: DateTimeRange) -> str:
-        return f"{timespan.start_datetime.strftime(DATE_FORMAT)}T{timespan.end_datetime.strftime(DATE_FORMAT)}"
-
-    def _get_tmp_filename(self, timespan: DateTimeRange) -> str:
-        base_filename = os.path.join(self.directory, self._get_filename(timespan))
-        return f"{base_filename}.tmp"
-
-    def _append_tmp(self, timespan: DateTimeRange, line: str):
-        tmp_filename = self._get_tmp_filename(timespan)
-        with open(tmp_filename, "a") as ftmp:
-            ftmp.write(f"{line}\n")
-
-    def _tmp_contains(self, timespan: DateTimeRange, key: str):
-        tmp_filename = self._get_tmp_filename(timespan)
-        if not os.path.isfile(tmp_filename):
+    # private helper methods
+    def _contains(self, timespan: DateTimeRange, data_type: str, path: str = None):
+        filename = self._get_filename(timespan)
+        if not os.path.isfile(filename):
             return False
-        with open(tmp_filename, "r") as ftmp:
-            lines = set(ftmp.read().splitlines())
-            return key in lines
 
+        with pyasdf.ASDFDataSet(filename, mpi=False, mode="r") as ccf_ds:
+            # source-receiver pair
+            exists = data_type in ccf_ds.auxiliary_data
+            if path is not None and exists:
+                return path in ccf_ds.auxiliary_data[data_type]
+            return exists
 
-def get_channel_pair_key(src_chan: Channel, rec_chan: Channel) -> str:
-    # E.g. CI.BAK.BHZ_CI.BAK.BHZ
-    return f"{src_chan.station}.{src_chan.type}_{rec_chan.station}.{rec_chan.type}"
+    def _add_aux_data(self, timespan: DateTimeRange, params: Dict, data_type: str, path: str, data: np.ndarray):
+        filename = self._get_filename(timespan)
+        with pyasdf.ASDFDataSet(filename, mpi=False) as ccf_ds:
+            ccf_ds.add_auxiliary_data(data=data, data_type=data_type, path=path, parameters=params)
+
+    def _get_station_pair(self, src_chan, rec_chan):
+        return f"{src_chan.station}_{rec_chan.station}"
+
+    def _get_filename(self, timespan: DateTimeRange) -> str:
+        return os.path.join(
+            self.directory,
+            f"{timespan.start_datetime.strftime(DATE_FORMAT)}T{timespan.end_datetime.strftime(DATE_FORMAT)}.h5",
+        )
