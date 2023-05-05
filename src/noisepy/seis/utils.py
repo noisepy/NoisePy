32ba4213036073122ec03aff1898e3ef4034a313
Carlos Garcia Jurado Suarez <carlosg@uw.edu>
2023-05-05 15:06:52 -0700

    Add docstrings

--- src/noisepy/seis/utils.py
+++ src/noisepy/seis/utils.py
@@ -9,6 +9,7 @@ ANON_ARG = "anon"
 
 
 def get_filesystem(path: str, storage_options: dict = {}) -> fsspec.AbstractFileSystem:
+    """Construct an fsspec filesystem for the given path"""
     url = urlparse(path)
     # default to anonymous access for S3 if the this is not already specified
     if url.scheme == S3_SCHEME and ANON_ARG not in storage_options:
@@ -17,7 +18,9 @@ def get_filesystem(path: str, storage_options: dict = {}) -> fsspec.AbstractFile
 
 
 def fs_join(path1: str, path2: str) -> str:
-    if path1.startswith(S3_SCHEME):
+    """Helper for joining two paths that can handle both S3 URLs and local file system paths"""
+    url = urlparse(path1)
+    if url.scheme == S3_SCHEME:
         return posixpath.join(path1, path2)
     else:
         return os.path.join(path1, path2)
