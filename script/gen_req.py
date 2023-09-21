8f258148a93ee4c5ebc7839838254c306160cbc2
carlosgjs <carlosgjs@users.noreply.github.com>
2023-09-21 16:13:26 -0700

    Fix source dist testing (#219)

--- script/gen_req.py
+++ script/gen_req.py
@@ -4,8 +4,9 @@ import sys
 
 import pkginfo
 
+aws_suffix = "; extra == 'aws'"
 wheel = sys.argv[1]
 reqs = pkginfo.get_metadata(wheel).requires_dist
-core_reqs = [req for req in reqs if "extra" not in req]
+core_reqs = [req.removesuffix(aws_suffix) for req in reqs if "extra" not in req or aws_suffix in req]
 for req in core_reqs:
     print(req)
