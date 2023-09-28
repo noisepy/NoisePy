ff969ff279836deba50acf1696b77d18c4b309e8
carlosgjs <carlosgjs@users.noreply.github.com>
2023-09-27 20:12:46 -0700

    Cleanup of misc. testing scripts (#226)
    * Update misc. scripts
    
    * Combine s1perf and test_cli

--- script/docker_dev.sh
+++ script/docker_dev.sh
@@ -5,5 +5,8 @@ rm ./dist/*.whl
 hatch build -t wheel
 python script/gen_req.py dist/*.whl > ./dist/_reqs.txt
 
-docker buildx build -f Dockerfile.dev --platform=linux/amd64,linux/arm64 -t ghcr.io/uw-ssec/dev-noisepy --push .
+# The build commands are faster, but single platform. Use buildx for multi-platform
+# docker buildx build -f Dockerfile.dev --platform=linux/amd64,linux/arm64 -t ghcr.io/uw-ssec/dev-noisepy --push .
 # docker buildx build -f script/write_speed/Dockerfile --platform=linux/amd64,linux/arm64 -t ghcr.io/uw-ssec/dev-noisepy --push .
+docker build -f Dockerfile.dev --platform=linux/amd64 -t ghcr.io/uw-ssec/dev-noisepy --push .
+# docker build -f script/write_speed/Dockerfile --platform=linux/amd64 -t ghcr.io/uw-ssec/dev-noisepy --push .
--- script/write_speed/write.py
+++ script/write_speed/write.py
@@ -1,13 +1,16 @@
 import io
 import json
 import logging
+import os
 import sys
 import tarfile
+import tempfile
 import time
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
+import pyasdf
 import zarr
 
 from noisepy.seis.tiledb import _TileDBHelper
@@ -23,7 +26,7 @@ A script to test the write speed of different storage options
 
 class Writer(ABC):
     def __init__(self, path: str) -> None:
-        self.path = fs_join(path, type(self).__name__)
+        self.path = fs_join(path, type(self).__name__) + "/"
         logger.info(f"Creating writer at {self.path}")
         self.storage_options = {"client_kwargs": {"region_name": "us-west-2"}}
         self.fs = get_filesystem(path, storage_options=self.storage_options)
@@ -154,6 +157,26 @@ class TileDBOneWriter(TileDBWriter):
         self.helper = _TileDBHelper(self.path, "w", 1, storage_options=self.storage_options)
 
 
+class ASDFWriter(Writer):
+    """
+    Writes a single tiledb array with all cross-correlations (tile size: 1 CC)
+    """
+
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+
+    def initialize(self):
+        self.temp = tempfile.gettempdir()
+
+    def write_override(self, id: int):
+        tmp = os.path.join(self.temp, f"{id}.h5")
+        with pyasdf.ASDFDataSet(tmp, mode="w", mpi=False, compression=None) as ds:
+            ds.add_auxiliary_data(data=self.data, parameters=self.params["channels"][0][4], data_type="CC", path="CC")
+
+        self.fs.put(tmp, fs_join(self.path, f"{id}.h5"))
+        os.remove(tmp)
+
+
 def test(nwrites: int, writer: Writer):
     # warmup
     WARMUP = 10
@@ -176,7 +199,7 @@ def test(nwrites: int, writer: Writer):
         f.write("\n".join([str(t) for t in times]))
 
 
-writer_types = [TileDBOneWriter, NumpyWriter, ZarrMultiArrayWriter, ZarrSingleArrayWriter, TileDBWriter]
+writer_types = [TileDBOneWriter, ASDFWriter, NumpyWriter, ZarrMultiArrayWriter, ZarrSingleArrayWriter, TileDBWriter]
 if __name__ == "__main__":
     writes = int(sys.argv[1])
     path = s3_path + str(writes)
