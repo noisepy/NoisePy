207b8c4d269e339df6d9f028eb1182e14c056377
Carlos Garcia Jurado Suarez <carlosg@uw.edu>
2023-10-03 17:30:39 -0700

    better testing

--- tests/test_hierarchicalstores.py
+++ tests/test_hierarchicalstores.py
@@ -6,6 +6,7 @@ from utils import range
 
 from noisepy.seis.hierarchicalstores import PairDirectoryCache
 from noisepy.seis.numpystore import NumpyArrayStore
+from noisepy.seis.zarrstore import ZarrStoreHelper
 
 
 def test_dircache():
@@ -49,7 +50,7 @@ def test_dircache():
         cache.add("src", "rec", [ts1, tsh1])
 
 
-paths = [
+numpy_paths = [
     (
         "some/path/CI.BAK/CI.ARV/2021_07_01_00_00_00T2021_07_02_00_00_00.tar.gz",
         ("CI.ARV", range(7, 1, 2)),
@@ -61,7 +62,25 @@ paths = [
 ]
 
 
-@pytest.mark.parametrize("path,expected", paths)
-def test_parse_path(path: str, expected: Tuple[str, DateTimeRange]):
+@pytest.mark.parametrize("path,expected", numpy_paths)
+def test_numpy_parse_path(path: str, expected: Tuple[str, DateTimeRange]):
     store = NumpyArrayStore("some/path", "r")
+    assert store.parse_path(path) == expected
+
+
+zarr_paths = [
+    (
+        "some/path/CI.BAK/CI.ARV/2021_07_01_00_00_00T2021_07_02_00_00_00/0.0.0",
+        ("CI.ARV", range(7, 1, 2)),
+    ),
+    ("some/path/CI.BAK/CI.BAK_CI.ARV/2021_07_01_00_00_00/0.0.0", None),
+    ("some/path/CI.BAK/CI.BAK/2021_07_01_00_00_00/.zgroup", None),
+    ("path/non_ts/0.0.0", None),
+    ("too_short/0.0.0", None),
+]
+
+
+@pytest.mark.parametrize("path,expected", zarr_paths)
+def test_zarr_parse_path(tmp_path, path: str, expected: Tuple[str, DateTimeRange]):
+    store = ZarrStoreHelper(str(tmp_path), "a")
     assert store.parse_path(path) == expected
