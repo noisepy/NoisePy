0ec07a0422421bdd140799aae896482b60e0069b
IshikaKhandelwal <ishikakhandelwal02@gmail.com>
2023-10-05 10:59:45 -0700

    "Added tests for SingleNodeScheduler"

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,5 +1,6 @@
 from noisepy.seis.scheduler import SingleNodeScheduler
 
+
 def test_single_node_scheduler_get_indices():
     single_node_scheduler = SingleNodeScheduler()
     items = ["a", "b", "c", "d"]
@@ -7,6 +8,7 @@ def test_single_node_scheduler_get_indices():
     indices = single_node_scheduler.get_indices(items)
 
     assert indices == expected_indices
+
 
 def test_synchronize():
     scheduler = SingleNodeScheduler()
