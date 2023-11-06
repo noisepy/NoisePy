b7b7e7c8a0c6f289bab2d8745a014c897dd21956
IshikaKhandelwal <IshikaKhandelwal02@gmail.com>
2023-11-06 10:55:19 -0800

    Migrated read docs page

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
     "sphinx.ext.graphviz",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
