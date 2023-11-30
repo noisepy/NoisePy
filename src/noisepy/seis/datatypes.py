4562d1d67f065fb57af59fa252318a88d7ac839c
IshikaKhandelwal <IshikaKhandelwal02@gmail.com>
2023-11-30 11:26:15 -0800

    Testing test_stations_file.py

--- src/noisepy/seis/datatypes.py
+++ src/noisepy/seis/datatypes.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-import sys
 import os
+import sys
 import typing
 from abc import ABC, abstractmethod
 from collections import defaultdict
@@ -158,7 +158,7 @@ class ConfigParameters(BaseModel):
     lomax: float = Field(default=-115.0, description="Download: maximum longitude")
     down_list: bool = Field(default=False, description="download stations from a pre-compiled list or not")
     net_list: List[str] = Field(default=["CI"], description="network list")
-    stations: List[str] = Field(default=[], description="station list")
+    stations: List[str] = Field(default=["*"], description="station list")
     channels: List[str] = Field(default=["BHE", "BHN", "BHZ"], description="channel list")
     # pre-processing parameters
     step: float = Field(default=450.0, description="overlapping between each cc_len (sec)")
@@ -231,25 +231,35 @@ class ConfigParameters(BaseModel):
     @property
     def dt(self) -> float:
         return 1.0 / self.samp_freq
-    
-    def load_stations(self,stations_list=None) -> List[str]:
+
+    def load_stations(self, stations_list=None) -> List[str]:
         if stations_list is None:
             # Load the list from the file
-            with open(self.stations_file, 'r') as file:
+            with open(self.stations_file, "r") as file:
                 self.stations = file.read().splitlines()
         else:
-            self.stations=stations_list
-        return []
+            self.stations = stations_list
+        return 0
+
 
     def save_stations(self, value: List[str]):
         if self.stations_file:
             # Save the list to the file
-            with open(self.stations_file, 'w') as file:
+            with open(self.stations_file, "w") as file:
                 file.write("\n".join(value))
             # Set stations field to Empty List
-            self.stations = []
-        else:
-            self.stations = value
+            # self.stations = []
+        return 0
+
+    # def save_stations(self, value: List[str]):
+    #     if self.stations_file:
+    #         # Save the list to the file
+    #         with open(self.stations_file, "w") as file:
+    #             file.write("\n".join(value))
+    #         # Set stations field to Empty List
+    #         self.stations = []
+    #     else:
+    #         self.stations = value
 
     @model_validator(mode="after")
     def validate(cls, m: ConfigParameters) -> ConfigParameters:
@@ -263,20 +273,32 @@ class ConfigParameters(BaseModel):
         validate_date(m.end_date, "end_date")
         if m.substack_len % m.cc_len != 0:
             raise ValueError(f"substack_len ({m.substack_len}) must be a multiple of cc_len ({m.cc_len})")
+
         
         # if m.stations_file and m.stations:
-        #     raise ValueError("If stations_file is set, stations must be empty.")
-        
-        def validate_stations_file(stations_file):
-            
-            if stations_file and not os.path.isfile(stations_file):
-                raise ValueError(f"{stations_file} is not a valid file path in stations_file.")
-    
-            return stations_file
+        #  raise ValueError("Only one of stations_file or stations can be specified, not both.")
 
-        validate_stations_file(m.stations_file)
+        if m.stations_file and not os.path.isfile(m.stations_file):
+            raise ValueError(f"{m.stations_file} is not a valid file path in stations_file.")
+
+        # def validate_stations_file(stations_file):
+        #     return stations_file
+
+        # validate_stations_file(m.stations_file)
         return m
     
+        # if m.stations_file and m.stations:
+        #     raise ValueError("If stations_file is set, stations must be empty.")
+
+        # def validate_stations_file(stations_file):
+        #     if stations_file and not os.path.isfile(stations_file):
+        #         raise ValueError(f"{stations_file} is not a valid file path in stations_file.")
+
+        #     return stations_file
+
+        # validate_stations_file(m.stations_file)
+        # return m
+
     # TODO: Remove once all uses of ConfigParameters have been converted to use strongly typed access
     def __getitem__(self, key):
         # Hack since pydantic model properties are nor part of the object's __dict__
