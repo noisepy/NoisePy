bf3303da39393361b209d78d1b7aa8060a5655c6
Kuan-Fu Feng <kffeng@psf-nvme01-prd-j375.ess.washington.edu>
2024-01-26 15:10:09 -0800

    Adding units tests for NCEDCS3DataStore

--- tests/test_scedc_s3store.py
+++ tests/test_scedc_s3store.py
@@ -6,7 +6,11 @@ from datetimerange import DateTimeRange
 from test_channelcatalog import MockCatalog
 
 from noisepy.seis.datatypes import Channel, ChannelType, Station
-from noisepy.seis.scedc_s3store import SCEDCS3DataStore, channel_filter
+from noisepy.seis.scedc_s3store import (
+    NCEDCS3DataStore,
+    SCEDCS3DataStore,
+    channel_filter,
+)
 
 timespan1 = DateTimeRange(datetime(2022, 1, 2, tzinfo=timezone.utc), datetime(2022, 1, 3, tzinfo=timezone.utc))
 timespan2 = DateTimeRange(datetime(2021, 2, 3, tzinfo=timezone.utc), datetime(2021, 2, 4, tzinfo=timezone.utc))
@@ -78,3 +82,36 @@ def test_filter():
     assert check(staX, "CHE") is False  # invalid channel name
     assert check(staZ, "BHE") is False  # invalid station
     assert check(staZ, "CHE") is False  # invalid station and channel name
+
+
+files_dates = [
+    ("AFD.NC.HHZ..D.2022.002", timespan1),
+    ("KCPB.NC.HHN..D.2021.034", timespan2),
+    ("LMC.NC.HHN..D.2023.152", timespan3),
+]
+
+
+@pytest.mark.parametrize("file,expected", files_dates)
+def test_parsefilename2(file: str, expected: DateTimeRange):
+    assert expected == NCEDCS3DataStore._parse_timespan(None, file)
+
+
+data_paths = [
+    (os.path.join(os.path.dirname(__file__), "./data/s3ncedc"), None),
+    ("s3://ncedc-pds/continuous_waveforms/NC/2022/2022.002/", None),
+    ("s3://ncedc-pds/continuous_waveforms/", timespan1),
+]
+
+
+read_channels = [
+    (NCEDCS3DataStore._parse_channel(None, "AFD.NC.HHZ..D.2022.002")),
+    (NCEDCS3DataStore._parse_channel(None, "NSP.NC.EHZ..D.2022.002")),
+    (NCEDCS3DataStore._parse_channel(None, "PSN.NC.EHZ..D.2022.002")),
+]
+
+
+@pytest.fixture(params=data_paths)
+def store2(request):
+    storage_options = {"s3": {"anon": True}}
+    (path, timespan) = request.param
+    return NCEDCS3DataStore(path, MockCatalog(), lambda ch: ch in read_channels, timespan, storage_options)
