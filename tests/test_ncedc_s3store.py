0585cc0e2d4e2b4241c22f03c0e4b4b8d0aae5e0
Kuan-Fu Feng <kffeng@psf-nvme01-prd-j375.ess.washington.edu>
2024-01-26 15:20:33 -0800

    Move units tests of NCEDCS3Store to a new test file.

--- tests/test_ncedc_s3store.py
+++ tests/test_ncedc_s3store.py
@@ -6,46 +6,46 @@ from datetimerange import DateTimeRange
 from test_channelcatalog import MockCatalog
 
 from noisepy.seis.datatypes import Channel, ChannelType, Station
-from noisepy.seis.scedc_s3store import SCEDCS3DataStore, channel_filter
+from noisepy.seis.scedc_s3store import NCEDCS3DataStore, channel_filter
 
 timespan1 = DateTimeRange(datetime(2022, 1, 2, tzinfo=timezone.utc), datetime(2022, 1, 3, tzinfo=timezone.utc))
 timespan2 = DateTimeRange(datetime(2021, 2, 3, tzinfo=timezone.utc), datetime(2021, 2, 4, tzinfo=timezone.utc))
 timespan3 = DateTimeRange(datetime(2023, 6, 1, tzinfo=timezone.utc), datetime(2023, 6, 2, tzinfo=timezone.utc))
 files_dates = [
-    ("CIGMR__LHN___2022002.ms", timespan1),
-    ("CIGMR__LHN___2021034.ms", timespan2),
-    ("AZCRY__BHE___2023152.ms", timespan3),
+    ("AFD.NC.HHZ..D.2022.002", timespan1),
+    ("KCPB.NC.HHN..D.2021.034", timespan2),
+    ("LMC.NC.HHN..D.2023.152", timespan3),
 ]
 
 
 @pytest.mark.parametrize("file,expected", files_dates)
-def test_parsefilename(file: str, expected: DateTimeRange):
-    assert expected == SCEDCS3DataStore._parse_timespan(None, file)
+def test_parsefilename2(file: str, expected: DateTimeRange):
+    assert expected == NCEDCS3DataStore._parse_timespan(None, file)
 
 
 data_paths = [
-    (os.path.join(os.path.dirname(__file__), "./data/s3scedc"), None),
-    ("s3://scedc-pds/continuous_waveforms/2022/2022_002/", None),
-    ("s3://scedc-pds/continuous_waveforms/", timespan1),
+    (os.path.join(os.path.dirname(__file__), "./data/s3ncedc"), None),
+    ("s3://ncedc-pds/continuous_waveforms/NC/2022/2022.002/", None),
+    ("s3://ncedc-pds/continuous_waveforms/", timespan1),
 ]
 
 
 read_channels = [
-    (SCEDCS3DataStore._parse_channel(None, "BKTHIS_LHZ00_2022002.ms")),
-    (SCEDCS3DataStore._parse_channel(None, "CIFOX2_LHZ___2022002.ms")),
-    (SCEDCS3DataStore._parse_channel(None, "CINCH__LHZ___2022002.ms")),
+    (NCEDCS3DataStore._parse_channel(None, "AFD.NC.HHZ..D.2022.002")),
+    (NCEDCS3DataStore._parse_channel(None, "NSP.NC.EHZ..D.2022.002")),
+    (NCEDCS3DataStore._parse_channel(None, "PSN.NC.EHZ..D.2022.002")),
 ]
 
 
 @pytest.fixture(params=data_paths)
-def store(request):
+def store2(request):
     storage_options = {"s3": {"anon": True}}
     (path, timespan) = request.param
-    return SCEDCS3DataStore(path, MockCatalog(), lambda ch: ch in read_channels, timespan, storage_options)
+    return NCEDCS3DataStore(path, MockCatalog(), lambda ch: ch in read_channels, timespan, storage_options)
 
 
 @pytest.mark.parametrize("channel", read_channels)
-def test_read(store: SCEDCS3DataStore, channel: str):
+def test_read(store: NCEDCS3DataStore, channel: str):
     chdata = store.read_data(timespan1, channel)
     assert chdata.sampling_rate == 1.0
     assert chdata.start_timestamp >= timespan1.start_datetime.timestamp()
@@ -53,7 +53,7 @@ def test_read(store: SCEDCS3DataStore, channel: str):
     assert chdata.data.size > 0
 
 
-def test_timespan_channels(store: SCEDCS3DataStore):
+def test_timespan_channels(store: NCEDCS3DataStore):
     timespans = store.get_timespans()
     assert len(timespans) == 1
     assert timespans[0] == timespan1
@@ -66,8 +66,8 @@ def test_timespan_channels(store: SCEDCS3DataStore):
 def test_filter():
     # filter for station 'staX' or 'staY' and channel type starts with 'B'
     f = channel_filter(["staX", "staY"], "B")
-    staX = Station("CI", "staX")
-    staZ = Station("CI", "staZ")
+    staX = Station("NC", "staX")
+    staZ = Station("NC", "staZ")
 
     def check(sta, ch_name):
         ch = Channel(ChannelType((ch_name)), sta)
